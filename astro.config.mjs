56e9e4377a1365290ee05b6e73aeb9abb639bbd4
jose onate <onarson@gmail.com>
2026-09-02 14:16:31 -0700

    optimized SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://joysomatic.com',
+  trailingSlash: 'always', // or 'never'
   output: 'server',
   adapter: cloudflare({
     imageService: 'passthrough',
   }),
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
     build: {
