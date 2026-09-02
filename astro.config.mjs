d159a7ab7aaac7efa504cb4a2e16c45092695199
jose onate <onarson@gmail.com>
2026-09-02 14:52:57 -0700

    inlineStylesheets always

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
   }),
   integrations: [sitemap()],
   build: {
-    inlineStylesheets: 'auto',
+    inlineStylesheets: 'always',
   },
   vite: {
     plugins: [tailwindcss()],
